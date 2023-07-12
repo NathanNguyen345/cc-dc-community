beb299ea3226208ae07a1b438403611cc866052a
Nathan Nguyen <nnguyen@Nathans-MacBook-Pro.local>
2023-07-11 22:17:31 -0700

    Added content for main home page

--- client/src/components/home/Homepage.js
+++ client/src/components/home/Homepage.js
@@ -2,16 +2,24 @@
 import React from "react";
 import styles from "./home.module.css";
 import splashImg from "../../assets/images/splashImg.png";
+import spotlghtImg from "../../assets/images/spotlightImg.png";
+import aboutImg from "../../assets/images/aboutImg.png";
 import Button from "../button/Button";
+import KeyFeatureCard from "../keyFeatureCard/KeyFeatureCard";
 
 const Homepage = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.gridContainer}>
+      {/* 
+      ********************************************************************************
+                                        Splash section                              
+      ********************************************************************************
+      */}
+      <div className={styles.splashGridContainer}>
         <div className={styles.title}>
           <h4>
-            Empowering Success: Unleash Your ACS/DC Potential with Our
-            All-in-One Solution
+            <span>Empowering Success:</span> Unleash Your ACS/DC Potential with
+            Our All-in-One Solution
           </h4>
         </div>
         <div className={styles.desc}>
@@ -22,12 +30,121 @@ const Homepage = () => {
             achievement.
           </p>
         </div>
-        <div className={styles.splashImg}>
+        <div className={`${styles.splashImg} ${styles.centerDiv}`}>
           <img src={splashImg} alt="splashImg" />
         </div>
 
         <div className={styles.button}>
           <Button text="Click to get started" />
+        </div>
+      </div>
+
+      {/*
+      ********************************************************************************
+                                        Spotlight section
+      ********************************************************************************
+      */}
+      <div className={styles.spotlightGridContainer}>
+        <div className={styles.spotlightTitle}>
+          <h4>
+            Unleash Our Versatile <span>API</span> Anywhere, Anytime!
+          </h4>
+        </div>
+        <div className={styles.spotlightDesc}>
+          <p>
+            Elevate your applications with ease using Acrobat Sign’s REST API.
+            Seamlessly integrate powerful Acrobat Sign functionality into your
+            own software, unlocking a world of possibilities for streamlined
+            document signing and management.
+          </p>
+        </div>
+        <div className={`${styles.spotlightImg} ${styles.centerDiv}`}>
+          <img src={spotlghtImg} alt="spotlightImg" />
+        </div>
+
+        <div className={styles.button}>
+          <Button text="Click to get started" />
+        </div>
+      </div>
+
+      {/*
+      ********************************************************************************
+                                        Key Feature section   
+      ********************************************************************************
+      */}
+      <div className={styles.keyfeatureGridContainer}>
+        <div className={`${styles.keyfeatureTitle} ${styles.centerDiv}`}>
+          <h4>
+            Transforming Ideas into Powerful <span>Solutions</span>
+          </h4>
+        </div>
+        <div className={styles.key1}>
+          <KeyFeatureCard
+            title={"Integrations"}
+            desc={"Document Cloud integrations to accelerate deployment."}
+            img={"gear"}
+          />
+        </div>
+        <div className={styles.key2}>
+          <KeyFeatureCard
+            title={"Sample"}
+            desc={
+              "Collection of Document Cloud code samples, examples, and templates"
+            }
+            img={"file"}
+          />
+        </div>
+        <div className={styles.key3}>
+          <KeyFeatureCard
+            title={"Tool"}
+            desc={
+              "A set of tools for Document Cloud to make developer lives easier"
+            }
+            img={"tool"}
+          />
+        </div>
+      </div>
+
+      {/*
+      ********************************************************************************
+                                        About section
+      ********************************************************************************
+      */}
+      <div className={styles.aboutGridContainer}>
+        <div className={styles.aboutTitle}>
+          <h4>
+            Meet the <span>Visionaries</span> Empowering this Transformation
+          </h4>
+        </div>
+        <div className={styles.aboutDesc}>
+          <p>
+            We specialize in developing custom solutions for Adobe products,
+            including Adobe Document Cloud. With expertise in applications like
+            Acrobat, Acrobat Sign, Acrobat Services, and Scan, we transform
+            document management and streamline workflows to empower your
+            productivity. Please note that the the Document Cloud developer
+            community maintains this repository.
+          </p>
+        </div>
+        <div className={`${styles.aboutImg} ${styles.centerDiv}`}>
+          <img src={aboutImg} alt="aboutImg" />
+        </div>
+      </div>
+
+      {/*
+      ********************************************************************************
+                                        Contact section
+      ********************************************************************************
+      */}
+      <div className={styles.contactGridContainer}>
+        <div className={styles.contactTitle}>
+          <h4>
+            Have Questions? <span>Contact</span> Us for Expert Assistance!
+          </h4>
+        </div>
+
+        <div className={styles.contactButton}>
+          <Button text="Contact Us" />
         </div>
       </div>
     </div>
---
+++ client/src/components/keyFeatureCard/KeyFeatureCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./keyFeatureCard.module.css";
+import FileSVG from "../../assets/icons/FileSVG";
+import GearSVG from "../../assets/icons/GearSVG";
+import ToolSVG from "../../assets/icons/ToolSVG";
+
+const KeyFeatureCard = ({ title, desc, img }) => {
+  const imgMap = {
+    file: <FileSVG />,
+    gear: <GearSVG />,
+    tool: <ToolSVG />,
+  };
+
+  return (
+    <div className={styles.flexboxContainer}>
+      <div className={styles.img}>{imgMap[img]}</div>
+      <div className={styles.title}>
+        <h1>{title}</h1>
+      </div>
+      <div className={styles.desc}>{desc}</div>
+    </div>
+  );
+};
+
+export default KeyFeatureCard;
