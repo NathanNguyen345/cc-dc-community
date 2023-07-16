2e436e75f41c0b4f7daebf38013d02d7557a2ee0
Nathan Nguyen <nnguyen@Nathans-MacBook-Pro-2.local>
2023-07-15 22:28:19 -0700

    Fix CSS bugs and added more styling to the homepage. Still need to work on responsiveness.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,12 @@ import Navbar from "./components/nav/Navbar";
 function App() {
   return (
     <div className="App">
+      <header>
+        <link
+          rel="stylesheet"
+          href="https://use.typekit.net/xpf5njd.css"
+        ></link>
+      </header>
       <div className="paddingContainer">
         <Navbar />
       </div>
--- client/src/components/home/Homepage.js
+++ client/src/components/home/Homepage.js
@@ -12,152 +12,154 @@ const Homepage = () => {
     <div className={styles.container}>
       <div className={styles.svgTop}></div>
 
-      {/* 
+      <div className={styles.containerInner}>
+        {/* 
       ********************************************************************************
                                         Splash Section                              
       ********************************************************************************
       */}
-      <div className={styles.splashGridContainer}>
-        <div className={styles.splashSectionFlex}>
-          <div className={styles.title}>
-            <h4>
-              <span>Empowering Success:</span> Unleash Your ACS/DC Potential
-              with Our All-in-One Solution
-            </h4>
+        <div className={styles.splashGridContainer}>
+          <div className={styles.splashSectionFlex}>
+            <div className={styles.title}>
+              <h4>
+                <span>Empowering Success:</span> Unleash Your ACS/DC Potential
+                with Our All-in-One Solution
+              </h4>
+            </div>
+            <div className={styles.desc}>
+              <p>
+                Unleash limitless potential with our trifecta of Samples,
+                Integrations, & Tools. Seamlessly integrate and leverage
+                cutting-edge Tools to propel your success and unlock new
+                horizons of achievement.
+              </p>
+            </div>
+            <div className={styles.button}>
+              <Button text="Click to get started" />
+            </div>
           </div>
-          <div className={styles.desc}>
-            <p>
-              Unleash limitless potential with our trifecta of Samples,
-              Integrations, & Tools. Seamlessly integrate and leverage
-              cutting-edge Tools to propel your success and unlock new horizons
-              of achievement.
-            </p>
-          </div>
-          <div className={styles.button}>
-            <Button text="Click to get started" />
-          </div>
-        </div>
 
-        <div className={styles.splashImgFlex}>
-          <div className={`${styles.splashImg} ${styles.centerDiv}`}>
-            <img src={splashImg} alt="splashImg" />
+          <div className={styles.splashImgFlex}>
+            <div className={`${styles.centerDiv}`}>
+              <img src={splashImg} alt="splashImg" />
+            </div>
           </div>
         </div>
-      </div>
-      {/*
+        {/*
       ********************************************************************************
                                         Spotlight Section
       ********************************************************************************
       */}
-      <div className={styles.spotlightContainer}>
-        <div className={styles.spotlightSectionFlex}>
-          <div className={styles.spotlightTitle}>
-            <h4>
-              Unleash Our Versatile <span>API</span> Anywhere, Anytime!
-            </h4>
+        <div className={styles.spotlightContainer}>
+          <div className={styles.spotlightSectionFlex}>
+            <div className={styles.spotlightTitle}>
+              <h4>
+                Unleash Our Versatile <span>API</span> Anywhere, Anytime!
+              </h4>
+            </div>
+            <div className={styles.spotlightDesc}>
+              <p>
+                Elevate your applications with ease using Acrobat Sign’s REST
+                API. Seamlessly integrate powerful Acrobat Sign functionality
+                into your own software, unlocking a world of possibilities for
+                streamlined document signing and management.
+              </p>
+            </div>
+            <div className={styles.buttonRight}>
+              <Button text="Click to get started" />
+            </div>
           </div>
-          <div className={styles.spotlightDesc}>
-            <p>
-              Elevate your applications with ease using Acrobat Sign’s REST API.
-              Seamlessly integrate powerful Acrobat Sign functionality into your
-              own software, unlocking a world of possibilities for streamlined
-              document signing and management.
-            </p>
-          </div>
-          <div className={styles.buttonRight}>
-            <Button text="Click to get started" />
-          </div>
-        </div>
 
-        <div className={styles.spotlightImgFlex}>
-          <div className={`${styles.spotlightImg} ${styles.centerDiv}`}>
-            <img src={spotlghtImg} alt="spotlightImg" />
+          <div className={styles.spotlightImgFlex}>
+            <div className={`${styles.centerDiv}`}>
+              <img src={spotlghtImg} alt="spotlightImg" />
+            </div>
           </div>
         </div>
-      </div>
-      {/*
+        {/*
       ********************************************************************************
                                         Key Feature Section   
       ********************************************************************************
       */}
-      <div className={styles.middeDiv}>
-        <div className={styles.keyfeatureGridContainer}>
-          <div className={`${styles.keyfeatureTitle} ${styles.centerDiv}`}>
-            <h4>
-              Transforming Ideas into Powerful <span>Solutions</span>
-            </h4>
-          </div>
-          <div className={styles.key1}>
-            <KeyFeatureCard
-              title={"Integrations"}
-              desc={"Document Cloud integrations to accelerate deployment."}
-              img={"gear"}
-            />
-          </div>
-          <div className={styles.key2}>
-            <KeyFeatureCard
-              title={"Sample"}
-              desc={
-                "Collection of Document Cloud code samples, examples, and templates"
-              }
-              img={"file"}
-            />
-          </div>
-          <div className={styles.key3}>
-            <KeyFeatureCard
-              title={"Tool"}
-              desc={
-                "A set of tools for Document Cloud to make developer lives easier"
-              }
-              img={"tool"}
-            />
+        <div className={styles.middeDiv}>
+          <div className={styles.keyfeatureGridContainer}>
+            <div className={`${styles.keyfeatureTitle} ${styles.centerDiv}`}>
+              <h4>
+                Transforming Ideas into Powerful <span>Solutions</span>
+              </h4>
+            </div>
+            <div className={styles.key1}>
+              <KeyFeatureCard
+                title={"Integrations"}
+                desc={"Document Cloud integrations to accelerate deployment."}
+                img={"gear"}
+              />
+            </div>
+            <div className={styles.key2}>
+              <KeyFeatureCard
+                title={"Sample"}
+                desc={
+                  "Collection of Document Cloud code samples, examples, and templates"
+                }
+                img={"file"}
+              />
+            </div>
+            <div className={styles.key3}>
+              <KeyFeatureCard
+                title={"Tool"}
+                desc={
+                  "A set of tools for Document Cloud to make developer lives easier"
+                }
+                img={"tool"}
+              />
+            </div>
           </div>
         </div>
-      </div>
-      {/*
+        {/*
       ********************************************************************************
                                         About Section
       ********************************************************************************
       */}
-      <div className={styles.aboutGridContainer}>
-        <div className={styles.aboutSectionFlex}>
-          <div className={styles.aboutTitle}>
-            <h4>
-              Meet the <span>Visionaries</span> Empowering this Transformation
-            </h4>
+        <div className={styles.aboutGridContainer}>
+          <div className={styles.aboutSectionFlex}>
+            <div className={styles.aboutTitle}>
+              <h4>
+                Meet the <span>Visionaries</span> Empowering this Transformation
+              </h4>
+            </div>
+            <div className={styles.aboutDesc}>
+              <p>
+                We specialize in developing custom solutions for Adobe products,
+                including Adobe Document Cloud. With expertise in applications
+                like Acrobat, Acrobat Sign, Acrobat Services, and Scan, we
+                transform document management and streamline workflows to
+                empower your productivity. Please note that the the Document
+                Cloud developer community maintains this repository.
+              </p>
+            </div>
           </div>
-          <div className={styles.aboutDesc}>
-            <p>
-              We specialize in developing custom solutions for Adobe products,
-              including Adobe Document Cloud. With expertise in applications
-              like Acrobat, Acrobat Sign, Acrobat Services, and Scan, we
-              transform document management and streamline workflows to empower
-              your productivity. Please note that the the Document Cloud
-              developer community maintains this repository.
-            </p>
-          </div>
-        </div>
 
-        <div className={styles.aboutImgFlex}>
-          <div className={`${styles.aboutImg} ${styles.centerDiv}`}>
-            <img src={aboutImg} alt="aboutImg" />
+          <div className={styles.aboutImgFlex}>
+            <div className={`${styles.centerDiv}`}>
+              <img src={aboutImg} alt="aboutImg" />
+            </div>
           </div>
         </div>
-      </div>
-      {/*
+        {/*
       ********************************************************************************
                                         Contact Section
       ********************************************************************************
       */}
-      <div className={styles.contactGridContainer}>
-        <div className={styles.contactTitle}>
-          <h4>
-            Have Questions? <span>Contact</span> Us for Expert Assistance!
-          </h4>
-        </div>
+        <div className={styles.contactGridContainer}>
+          <div className={styles.contactTitle}>
+            <h4>
+              Have Questions? <span>Contact</span> Us for Expert Assistance!
+            </h4>
+          </div>
 
-        <div className={styles.contactButton}>
-          <Button text="Contact Us" />
+          <div className={styles.contactButton}>
+            <Button text="Contact Us" />
+          </div>
         </div>
       </div>
       <div className={styles.svgBottom}></div>
