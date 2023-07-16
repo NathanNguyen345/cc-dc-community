9c3c12ed2016ac9e68e215322e2f2f7783f54c28
Nathan Nguyen <nnguyen@Nathans-MBP.localdomain>
2023-07-15 19:16:57 -0700

    Fix CSS layout, overflows, and background images

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
-// import Homepage from "./components/home/Homepage";
 import Navbar from "./components/nav/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="paddingContainer">
         <Navbar />
-        {/* <Homepage /> */}
-      </header>
+      </div>
     </div>
   );
 }
